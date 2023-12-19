2abb609be617c8c3c9b13391b0bd98a8dc80f54c
M. Enes Celik <m96celik@gmail.com>
2023-12-19 17:29:05 +0300

    data-binding-checkbox&radio

--- thymeleafdemo/thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/Controller/StudentController.java
+++ thymeleafdemo/thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/Controller/StudentController.java
@@ -1,14 +1,23 @@
 package com.luv2code.springboot.thymeleafdemo.Controller;
 
 import com.luv2code.springboot.thymeleafdemo.Model.Student;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class StudentController {
+
+    @Value("${countries}")
+    private List<String> countries;
+
+    @Value("${languages}")
+    private List<String> languages;
 
     @GetMapping("/showStudentForm")
     public String showStudForm(Model theModel) {
@@ -18,6 +27,12 @@ public class StudentController {
 
         // add student object to the model
         theModel.addAttribute("student", theStudent);
+
+        // add the list of countries to model
+        theModel.addAttribute("countries", countries);
+
+        // add list of languages to model
+        theModel.addAttribute("languages", languages);
 
         return "student-form";
     }
--- thymeleafdemo/thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/Model/Student.java
+++ thymeleafdemo/thymeleafdemo/src/main/java/com/luv2code/springboot/thymeleafdemo/Model/Student.java
@@ -1,16 +1,45 @@
 package com.luv2code.springboot.thymeleafdemo.Model;
 
 
+import java.util.List;
+
 public class Student {
 
     private String fName;
     private String lName;
+    private String country;
+    private String favoriteLanguage;
+    private List<String> favoriteOS;
 
     public Student() { }
 
     public Student(String fName, String lName) {
         this.fName = fName;
         this.lName = lName;
+    }
+
+    public List<String> getFavoriteOS() {
+        return favoriteOS;
+    }
+
+    public void setFavoriteOS(List<String> favoriteOS) {
+        this.favoriteOS = favoriteOS;
+    }
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 
     public String getfName() {
@@ -34,6 +63,7 @@ public class Student {
         return "Student{" +
                 "fName='" + fName + '\'' +
                 ", lName='" + lName + '\'' +
+                ", country='" + country + '\'' +
                 '}';
     }
 }
