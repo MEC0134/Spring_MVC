20dca776cf52699cb901f48470502ddb01e3a952
M. Enes Celik <m96celik@gmail.com>
2023-12-21 15:20:56 +0300

    working-with-Integer-fields

--- validationdemo/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Controllers/CustomerController.java
+++ validationdemo/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Controllers/CustomerController.java
@@ -2,15 +2,30 @@ package com.luv2code.springdemo.mvc.Controllers;
 
 import com.luv2code.springdemo.mvc.Model.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
+
+    // add an initbinder to convert trim input strings
+    @InitBinder
+    public void InitBinder(WebDataBinder dataBinder) {
+
+        // removes leading and trailing white spaces, true will trim to null if all white
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        // applying stringTrimmerEditor for every String class
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+
+    }
 
     @GetMapping("/customerForm")
     public String customerForm(Model theModel) {
@@ -25,12 +40,15 @@ public class CustomerController {
             @Valid @ModelAttribute("customer") Customer theCustomer,
             BindingResult theBindingResult) {
 
+        System.out.println("Binding Result - " + theBindingResult.toString());
+
         if(theBindingResult.hasErrors()) {
             return "customer-form";
         } else {
             return "customer-confirmation";
         }
-
     }
+
+
 
 }
--- validationdemo/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Model/Customer.java
+++ validationdemo/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Model/Customer.java
@@ -1,19 +1,40 @@
 package com.luv2code.springdemo.mvc.Model;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
-    @NotNull()
-    @Size(min = 2, message = "Field is required and min length must be 2 characters!")
     private String firstName;
 
-    @NotNull()
+    @NotNull(message = "Field Required")
     @Size(min = 2, message = "Field is required and min length must be 2 characters!")
     private String lastName;
 
+    @NotNull(message = "Field Required")
+    @Min(value = 0, message = "must be greater than or equal to 0")
+    @Max(value = 10, message = "must be less than or equal to 10")
+    private Integer itemAmount;
 
+    @Pattern(regexp = "[a-zA-Z0-9]{6}", message = "Only 6 chars/digits")
+    private String postalCode;
+
+
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public Integer getItemAmount() {
+        return itemAmount;
+    }
+
+    public void setItemAmount(Integer itemAmount) {
+        this.itemAmount = itemAmount;
+    }
 
     public String getFirstName() {
         return firstName;
@@ -30,4 +51,6 @@ public class Customer {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
+
+
 }
