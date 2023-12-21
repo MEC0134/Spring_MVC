b7292832b5751ae6d2929ef83cd2ef6d52bafa72
M. Enes Celik <m96celik@gmail.com>
2023-12-21 19:16:10 +0300

    custom-validation-added

--- validationdemo/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Model/Customer.java
+++ validationdemo/validationdemo/src/main/java/com/luv2code/springdemo/mvc/Model/Customer.java
@@ -1,5 +1,6 @@
 package com.luv2code.springdemo.mvc.Model;
 
+import com.luv2code.springdemo.mvc.validation.CourseCode;
 import jakarta.validation.constraints.*;
 
 public class Customer {
@@ -18,7 +19,19 @@ public class Customer {
     @Pattern(regexp = "[a-zA-Z0-9]{6}", message = "Only 6 chars/digits")
     private String postalCode;
 
+    @CourseCode(value = "MEC", message = "code must start with MEC")
+    private String courseCode;
 
+
+    // Getters & Setters
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
 
     public String getPostalCode() {
         return postalCode;
---
+++ validationdemo/validationdemo/src/main/java/com/luv2code/springdemo/mvc/validation/CourseCodeConstraintValidator.java
@@ -0,0 +1,29 @@
+package com.luv2code.springdemo.mvc.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class CourseCodeConstraintValidator implements ConstraintValidator<CourseCode, String> {
+
+    private String coursePrefix;
+
+    @Override
+    public void initialize(CourseCode theCourseCode) {
+        coursePrefix = theCourseCode.value();
+    }
+
+    @Override
+    public boolean isValid(String theCourseCode, ConstraintValidatorContext constraintValidatorContext) {
+
+        boolean result;
+
+        // does prefix start with LUV
+        if(theCourseCode != null) {
+            result = theCourseCode.startsWith(coursePrefix);
+        } else {
+            result = true;
+        }
+
+        return result;
+    }
+}
